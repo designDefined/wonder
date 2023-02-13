f14a13a59d667de6aa94c5cb31a36b8db22c8766
designDefined <designdefined.by@gmail.com>
2023-02-14 05:51:55 +0900

    #5/feat/initialize home (#6)
    * setup sample home components
    
    * add Header
    
    * update PR and Issue template

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,36 @@
 import styles from "./Main.module.scss";
 import classNames from "classnames/bind";
+import Header from "../components/common/Header";
 
 const cx = classNames.bind(styles);
 
 const Main = () => {
   return (
-    <article className={cx("Main")}>
-      <div className={cx("container")}></div>
-    </article>
+    <main className={cx("Main")}>
+      <Header />
+      <article className={cx("bannerCarousel")}>banner image</article>
+      <article className={cx("introduction")}>
+        <div>왜 원더여야 할까요?</div>
+        <section className={cx("advantage")}>
+          귀여운 일러스트 <br />+<br /> 장점1
+        </section>
+        <section className={cx("advantage")}>
+          귀여운 일러스트 <br />+<br /> 장점2
+        </section>
+        <button className={cx("toSignIn")}>회원가입 하러 가기</button>
+      </article>
+      <article className={cx("recent")}>
+        <div className={cx("label")}>지금 올라온 이벤트</div>
+        <ul className={cx("eventList")}>
+          <li className={cx("event")}>이벤트 1</li>
+        </ul>
+      </article>
+      <div className={cx("buttons")}>
+        <button className={cx("instagram")}>원더 인스타그램 보러 가기</button>
+        <button className={cx("guide")}>원더 창작자 가이드</button>
+      </div>
+      <footer className={cx("footer")}>footer</footer>
+    </main>
   );
 };
 
